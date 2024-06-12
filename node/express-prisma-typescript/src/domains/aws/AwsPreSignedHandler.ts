85f3f6e2aa5f08e52424d7c882a64697a485af5d
Kuha <khalil.stessens@ing.austral.edu.ar>
2024-06-12 14:07:39 -0300

    aws wip

---
+++ node/express-prisma-typescript/src/domains/aws/AwsPreSignedHandler.ts
@@ -0,0 +1,22 @@
+import AWS from 'aws-sdk'
+import { v4 as uuidv4 } from 'uuid'
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+})
+
+const BUCKET_NAME = process.env.BUCKET_NAME
+
+export const generatePresignedUrl = async (userId: string, fileType: string): Promise<string> => {
+  const key = `profile-pictures/${userId}/${uuidv4()}.${fileType}`
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: key,
+    Expires: 60 * 5, // URL valid for 5 minutes
+    ContentType: `image/${fileType}`
+  }
+
+  return await s3.getSignedUrlPromise('putObject', params)
+}
--- node/express-prisma-typescript/src/domains/user/controller/user.controller.ts
+++ node/express-prisma-typescript/src/domains/user/controller/user.controller.ts
@@ -7,6 +7,7 @@ import { db } from '@utils'
 
 import { UserRepositoryImpl } from '../repository'
 import { UserService, UserServiceImpl } from '../service'
+import { generatePresignedUrl } from '@domains/aws/AwsPreSignedHandler'
 
 export const userRouter = Router()
 
@@ -130,4 +131,79 @@ userRouter.delete('/', async (req: Request, res: Response) => {
   await service.deleteUser(userId)
 
   return res.status(HttpStatus.OK)
+})
+
+/**
+ * @swagger
+ * /user/{userId}/presigned-url:
+ *   get:
+ *     summary: Generate a pre-signed URL for file upload
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the user
+ *       - in: query
+ *         name: fileType
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Type of the file to be uploaded
+ *     responses:
+ *       200:
+ *         description: Pre-signed URL generated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 url:
+ *                   type: string
+ *                   description: The pre-signed URL
+ *               example:
+ *                 url: "https://example.com/presigned-url"
+ *       400:
+ *         description: Bad request, file type is required and must be a string
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *               example:
+ *                 message: "File type is required and must be a string"
+ *       500:
+ *         description: Could not generate pre-signed URL
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 error:
+ *                   type: object
+ *               example:
+ *                 message: "Could not generate pre-signed URL"
+ *                 error: {}
+ */
+userRouter.get('/:userId/presigned-url', async (req: Request, res: Response) => {
+  const { userId } = res.locals.context
+  const { fileType } = req.query
+
+  if (typeof fileType !== 'string') {
+    return res.status(400).json({ message: 'File type is required and must be a string' })
+  }
+
+  try {
+    const url = await generatePresignedUrl(userId, fileType)
+    res.json({ url })
+  } catch (error) {
+    res.status(500).json({ message: 'Could not generate pre-signed URL', error })
+  }
 })
